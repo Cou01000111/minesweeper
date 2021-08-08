c5a6599d0d782ffe726c0df9598dc44eb0afc14f
Cou01000111 <courange.c@gmail.com>
2021-08-08 19:03:10 +0900

    complete main game

--- src/main.c
+++ src/main.c
@@ -9,39 +9,38 @@
 #include "c_logger.h"
 #include "c_logger_value.h"
 #define N_MINE 10
-#define BD_WD 20
-#define BD_HT 10
+#define BD_WD 10
+#define BD_HT 5
 #define ESC 0x1b
 #define KEY_UP 0x48
 #define KEY_DOWN 0x50
 #define KEY_RIGHT 0x4d
 #define KEY_LEFT 0x4b
+#define IS_UPPER_SIDE(y) (y == 0)
+#define IS_LOWER_SIDE(y) (y == BD_HT - 1)
+#define IS_LEFT_SIDE(x) (x == 0)
+#define IS_RIGHT_SIDE(x) (x == BD_WD - 1)
 
 // 端末設定保存用大域変数
 struct termios CookedTermIos;  // cooked モード用
 struct termios RawTermIos;     // raw モード用
 #define BOM -1
 
-typedef enum { CENTER,
-               UP,
-               UP_RIGHT,
-               RIGHT,
-               DOWN_RIGHT,
-               DOWN,
-               DOWN_LEFT,
-               LEFT,
-               UP_LEFT } directions;
+//y,x
+int vertical_horizontal_oblique[8][2] = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {-1, -1}, {0, -1}};
+int vertical_horizontal[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
 typedef struct {
     int x;
     int y;
 } t_cursor;
 
-//めくったかどうかがある{(めくった)1,(めくってない)0}
+//めくったかどうかがある{(めくった)1,(めくってない)0,(pin)2}
 int g_ground_bd[BD_HT][BD_WD];
 t_cursor g_cursor;
 //{0,1,2,3,4,5,6,7,8,bom(-1)}
 int g_under_bd[BD_HT][BD_WD];
+int g_dig_bord[BD_HT][BD_WD];
 
 int push_buf(char *buf, char *str) {
     strncpy(buf, str, strlen(str));
@@ -51,7 +50,7 @@ int push_buf(char *buf, char *str) {
 bool render() {
     tcsetattr(STDIN_FILENO, 0, &CookedTermIos);
     system("clear");
-    char buf[1024];
+    char buf[2048];
     int buf_i = 0;
     for (size_t i = 0; i < BD_HT; i++) {
         for (size_t j = 0; j < BD_WD; j++) {
@@ -59,17 +58,19 @@ bool render() {
                 printf("cursor:x:%d,y:%d\n", g_cursor.x, g_cursor.y);
                 buf_i += push_buf(&buf[buf_i], "\x1b[7m");
             }
-            if (!g_ground_bd[i][j])
-                strncpy(&buf[buf_i], "O", 1);
+            if (g_ground_bd[i][j] == 0)
+                buf_i += push_buf(&buf[buf_i], "\x1b[33mO");
+            else if (g_ground_bd[i][j] == 2)
+                buf_i += push_buf(&buf[buf_i], "\x1b[35mP");
             else if (g_under_bd[i][j] == BOM)
-                strncpy(&buf[buf_i], "x", 1);
-            else if (g_under_bd[i][j] >= 0 && g_under_bd[i][j] <= 8)
+                buf_i += push_buf(&buf[buf_i], "\x1b[31mB");
+            else if (g_under_bd[i][j] >= 0 && g_under_bd[i][j] <= 8) {
                 sprintf(&buf[buf_i], "%d", g_under_bd[i][j]);
-            else {
+                buf_i += 1;
+            } else {
                 c_logger_error_log("invalid g_ground_bd");
                 return false;
             }
-            buf_i++;
             buf_i += push_buf(&buf[buf_i], "\x1b[0m");
         }
         buf_i += push_buf(&buf[buf_i], "\n");
@@ -89,14 +90,24 @@ void init_boards() {
     g_cursor.y = 0;
 }
 
+void init_dig_bord() {
+    for (size_t i = 0; i < BD_HT; i++)
+        for (size_t j = 0; j < BD_WD; j++) g_dig_bord[i][j] = 0;
+}
+
 void add_bom() {
     for (size_t i = 0; i < N_MINE; i++) {
         srand((int)time(NULL));
-        int h = rand() % BD_HT, w = rand() % BD_HT;
+        int h = rand() % BD_HT, w = rand() % BD_WD;
         while (g_under_bd[h][w] == BOM)
-            h = rand() % BD_HT, w = rand() % BD_HT;
+            h = rand() % BD_HT, w = rand() % BD_WD;
         g_under_bd[h][w] = BOM;
     }
+}
+
+int check_mine(int y, int x) {
+    if (x < 0 || y < 0 || BD_WD <= x || BD_HT <= y) return false;
+    return g_under_bd[y][x] == BOM;
 }
 
 void add_bom_count() {
@@ -104,26 +115,11 @@ void add_bom_count() {
         for (size_t x = 0; x < BD_WD; x++) {
             if (g_under_bd[y][x] == BOM)
                 continue;
-            //up(y > 0)
-            if (y > 0 && g_under_bd[y - 1][x] == BOM) g_under_bd[y][x]++;
-            //up right(y > 0 && x < BD_WD)
-            if (y > 0 && x < BD_WD && g_under_bd[y - 1][x + 1] == BOM) g_under_bd[y][x]++;
-            //right(x < BD_WD)
-            if (x < BD_WD && g_under_bd[y][x + 1] == BOM) g_under_bd[y][x]++;
-            //down right(y < BD_HT&&x < BD_WD)
-            if (y < BD_HT && x < BD_WD && g_under_bd[y + 1][x + 1] == BOM) g_under_bd[y][x]++;
-            //down(y < BD_HT)
-            if (y < BD_HT && g_under_bd[y + 1][x] == BOM) g_under_bd[y][x]++;
-            //down left(y < BD_HT&&x > 0)
-            if (y < BD_HT && x > 0 && g_under_bd[y + 1][x - 1] == BOM) g_under_bd[y][x]++;
-            //left(x > 0)
-            if (x > 0 && g_under_bd[y][x - 1] == BOM) g_under_bd[y][x]++;
-            //up left(y < BD_HT&&x > 0)
-            if (y < BD_HT && x > 0 && g_under_bd[y - 1][x - 1] == BOM) g_under_bd[y][x]++;
+            for (size_t i = 0; i < 8; i++)
+                g_under_bd[y][x] += check_mine(y + vertical_horizontal_oblique[i][1], x + vertical_horizontal_oblique[i][0]);
         }
     }
 }
-
 //成功なら1,失敗なら0
 bool init_g_values() {
     init_boards();
@@ -132,30 +128,23 @@ bool init_g_values() {
     return true;
 }
 
-void dig(int y, int x, directions dirc) {
-    char str[1000];
-    sprintf(str, "y:%d,x:%d", y, x);
-    c_logger_debug_log(str);
-    g_ground_bd[y][x] = 1;
-    //up(y > 0)
-    if (dirc != UP && y > 0 && g_under_bd[y - 1][x] == 0) dig(y - 1, x, DOWN);
-    //up right(y > 0 && x < BD_WD)
-    if (dirc != UP_RIGHT && y > 0 && x < BD_WD && g_under_bd[y - 1][x + 1] == 0) dig(y - 1, x + 1, DOWN_LEFT);
-    //right(x < BD_WD)
-    if (dirc != RIGHT && x < BD_WD && g_under_bd[y][x + 1] == 0) dig(y, x + 1, LEFT);
-    //down right(y < BD_HT&&x < BD_WD)
-    if (dirc != DOWN_RIGHT && y < BD_HT && x < BD_WD && g_under_bd[y + 1][x + 1] == 0) dig(y + 1, x + 1, UP_LEFT);
-    //down(y < BD_HT)
-    if (dirc != DOWN && y < BD_HT && g_under_bd[y + 1][x] == 0) dig(y + 1, x, UP);
-    //down left(y < BD_HT&&x > 0)
-    if (dirc != DOWN_LEFT && y < BD_HT && x > 0 && g_under_bd[y + 1][x - 1] == 0) dig(y + 1, x - 1, UP_RIGHT);
-    //left(x > 0)
-    if (dirc != LEFT && x > 0 && g_under_bd[y][x - 1] == 0) dig(y, x - 1, RIGHT);
-    //up left(y < BD_HT&&x > 0)
-    if (dirc != UP_LEFT && y < BD_HT && x > 0 && g_under_bd[y - 1][x - 1] == 0) dig(y - 1, x - 1, DOWN_RIGHT);
+void dig(int y, int x) {
+    if (x < 0 || y < 0 || BD_WD <= x || BD_HT <= y || g_ground_bd[y][x] == 1 || g_under_bd[y][x] == BOM)
+        return;
+    else {
+        g_ground_bd[y][x] = 1;
+        if ((g_under_bd[y][x] >= 1 && g_under_bd[y][x] <= 8) == false)
+            for (size_t i = 0; i < 4; i++)
+                dig(y + vertical_horizontal[i][1], x + vertical_horizontal[i][0]);
+    }
+}
+
+void pin(int y, int x) {
+    g_ground_bd[y][x] = 2;
 }
 
 int main() {
+    if (N_MINE > BD_HT * BD_WD) return 0;
     // 初期状態の端末設定 (cooked モード) を取得・保存する．
     tcgetattr(STDIN_FILENO, &CookedTermIos);
     // raw モードの端末設定を作成・保存する．
@@ -174,10 +163,10 @@ int main() {
                     g_cursor.y += (g_cursor.y > 0) ? -1 : 0;
                     break;
                 case 'B':
-                    g_cursor.y += (g_cursor.y < BD_HT - 1) ? 1 : 0;
+                    g_cursor.y += (g_cursor.y < (BD_HT - 1)) ? 1 : 0;
                     break;
                 case 'C':
-                    g_cursor.x += (g_cursor.x < BD_WD - 1) ? 1 : 0;
+                    g_cursor.x += (g_cursor.x < (BD_WD - 1)) ? 1 : 0;
                     break;
                 case 'D':
                     g_cursor.x += (g_cursor.x > 0) ? -1 : 0;
@@ -186,9 +175,12 @@ int main() {
             render();
         } else if (c[0] == ESC)
             break;
-        else if (c[0] == 32) {
-            dig(g_cursor.y, g_cursor.x, CENTER);
-        }
+        else if (c[0] == 32 || c[0] == 'x')
+            dig(g_cursor.y, g_cursor.x);
+        else if (c[0] == 'z')
+            pin(g_cursor.y, g_cursor.x);
+        else if (c[0] == 'c')
+            pin(g_cursor.y, g_cursor.x);
     }
     /*
     初期画面
